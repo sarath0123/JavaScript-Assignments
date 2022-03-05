b56e44a3762117041a577bd5874d15056f372155
sarath0123 <sarathsurendran7@gmail.com>
2022-03-05 13:18:26 +0530

    Createobject

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,11 +1,22 @@
 function CreateObject(arr) {
     // Write your code here
-
+    var keys = [];
+    var values = [];
+    var d={};
     for(let i=0; i<arr.length; i++)
     {
-        
+        if(i%2==0)
+        {
+            keys.push(arr[i]);
+        }
+        else
+        {
+            values.push(arr[i]);
+        }
     }
-
+    
+    keys.forEach((key, i) => d[key] = values[i]);
+    return d
 }
 
 module.exports = CreateObject;
